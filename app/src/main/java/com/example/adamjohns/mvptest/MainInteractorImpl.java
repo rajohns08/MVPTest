a09df8fd599f55a5ae2c377b17bf1da4b444eaa4
Adam Johns <johnsar@us.ibm.com>
2015-06-16 14:10:50 -0500

    interactor

---
+++ app/src/main/java/com/example/adamjohns/mvptest/MainInteractorImpl.java
@@ -0,0 +1,29 @@
+package com.example.adamjohns.mvptest;
+
+import com.example.adamjohns.mvptest.Retrofit.RestManager;
+import com.google.gson.JsonElement;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainInteractorImpl implements MainInteractor{
+
+    @Override
+    public void getData(final OnDataRetrievedListener listener) {
+
+        RestManager.getInstance().getRandomPlaceholderData(new Callback<JsonElement>() {
+            @Override
+            public void success(JsonElement jsonElement, Response response) {
+                listener.onSuccess(jsonElement.toString());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                listener.onFailure();
+            }
+        });
+
+    }
+
+}
